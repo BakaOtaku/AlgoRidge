3b73a6fc99405c7b8f08adeb48625347eaf871ac
Aman Raj <archanaamanraj@gmail.com>
2022-01-08 19:15:40 +0530

    fix: contract data read

---
+++ app.js
@@ -0,0 +1,82 @@
+const algosdk = require('algosdk');
+const { mnemonic, approvalSource, clearSource } = require("./constant");
+
+// client init
+const baseServer = 'https://testnet-algorand.api.purestake.io/ps2'
+const port = '';
+const token = {
+  'X-API-Key': 'bUemI5ZIty4XI3s7hxZ785dUdxvWhV9wJXOySBK8'
+}
+const algodClient = new algosdk.Algodv2(token, baseServer, port);
+
+const contractReadFunc = async () => {
+  const { addr: senderAddress } = algosdk.mnemonicToSecretKey(mnemonic);
+  readGlobalState(algodClient, senderAddress, 57209087);
+}
+
+const contractUpdateFunc = async () => {
+  const { addr: senderAddress } = algosdk.mnemonicToSecretKey(mnemonic);
+  
+}
+
+const deployContract = async () => {
+  // get account from mnemonic
+  const { sk: senderPvtKey, addr: senderAddress } = algosdk.mnemonicToSecretKey(mnemonic);
+  const compiledApprovalContract = await algodClient.compile(approvalSource).do();
+  const compiledClearContract = await algodClient.compile(clearSource).do();
+  // get node suggested parameters
+  const params = await algodClient.getTransactionParams().do();
+  // declare onComplete as NoOp
+  // onComplete = algodClient.OnApplicationComplete.NoOpOC;
+  // create unsigned transaction
+  const txn = algosdk.makeApplicationCreateTxnFromObject({
+    suggestedParams: {
+      ...params,
+    },
+    from: senderAddress,
+    numLocalByteSlices: 4,
+    numGlobalByteSlices: 2,
+    numLocalInts: 0,
+    numGlobalInts: 2,
+    approvalProgram: new Uint8Array(Buffer.from(compiledApprovalContract.result, "base64")),
+    clearProgram: new Uint8Array(Buffer.from(compiledClearContract.result, "base64")),
+    onComplete: 0,
+  });
+  const signedTxn = algosdk.signTransaction(txn, senderPvtKey);
+  let sendTx = await algodClient.sendRawTransaction(signedTxn.blob).do();
+  console.log(sendTx);
+  // get appId by txId
+}
+
+// read local state of application from user account
+async function readLocalState(client, account, appId) {
+  let accountInfoResponse = await client.accountInformation(account).do();
+  for (let i = 0; i < accountInfoResponse['created-apps'].length; i++) {
+    if (accountInfoResponse['created-apps'][i].id == appId) {
+      // console.log(accountInfoResponse['created-apps'][i])
+      console.log("User's local state:");
+      for (let n = 0; n < accountInfoResponse['created-apps'][i]['params']['global-state'].length; n++) {
+        console.log(accountInfoResponse['created-apps'][i]['params']['global-state'][n]);
+      }
+    }
+  }
+}
+
+// read global state of application
+async function readGlobalState(client, account, index) {
+  let accountInfoResponse = await client.accountInformation(account).do();
+  for (let i = 0; i < accountInfoResponse['created-apps'].length; i++) {
+    if (accountInfoResponse['created-apps'][i].id == index) {
+      console.log(accountInfoResponse['created-apps'][i])
+      console.log("Application's global state:");
+      for (let n = 0; n < accountInfoResponse['created-apps'][i]['params']['global-state'].length; n++) {
+        console.log(accountInfoResponse['created-apps'][i]['params']['global-state'][n]);
+      }
+    }
+  }
+}
+
+
+// deployContract()
+contractReadFunc()
+contractUpdateFunc()
