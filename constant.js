3032944a723689747ad18fe652d17d180985709e
fuzious <arpitsrivastava2012@gmail.com>
2022-01-11 14:54:46 +0530

    fix

--- constant.js
+++ constant.js
@@ -1,61 +1,71 @@
-const mnemonic = "during transfer welcome cram kitten bronze sorry blush empty acoustic manual keep draw jungle inner merry debate antenna range arrange burden drama bubble absorb patrol"
+const mnemonic = "noodle thing just rabbit opinion never estate effort street goddess earth kidney whip rhythm sea blood abstract glory similar attend trouble mask bunker abstract rubber"
 const approvalSource = `
 #pragma version 5
 txn ApplicationID
 int 0
 ==
-bnz main_l22
+bnz main_l24
 txn OnCompletion
 int OptIn
 ==
-bnz main_l21
+bnz main_l23
 txn OnCompletion
 int CloseOut
 ==
-bnz main_l20
+bnz main_l22
 txn OnCompletion
 int UpdateApplication
 ==
-bnz main_l19
+bnz main_l21
 txn OnCompletion
 int DeleteApplication
 ==
-bnz main_l18
+bnz main_l20
 txn OnCompletion
 int NoOp
 ==
 bnz main_l7
 err
 main_l7:
-global GroupSize
-int 1
-==
 txna ApplicationArgs 0
 byte "AddBot"
 ==
-&&
-bnz main_l17
+bnz main_l19
 txna ApplicationArgs 0
 byte "addliquidity"
 b==
-bnz main_l16
+bnz main_l18
 txna ApplicationArgs 0
 byte "removeliquidity"
 b==
-bnz main_l15
-txna ApplicationArgs 0
+bnz main_l17
+gtxna 0 ApplicationArgs 0
 byte "useBridge"
 b==
-bnz main_l14
+bnz main_l16
 txna ApplicationArgs 0
 byte "releasePayment"
 b==
-bnz main_l13
+bnz main_l15
+txna ApplicationArgs 0
+byte "setupdev"
+b==
+bnz main_l14
 err
-main_l13:
-txn Sender
+main_l14:
 byte "bot"
-app_local_get
+txn Sender
+app_global_put
+byte "admin"
+txn Sender
+app_global_put
+callsub sub4
+return
+main_l15:
+byte "bot"
+app_global_get
+txn Sender
+==
 assert
 txna ApplicationArgs 1
 btoi
@@ -66,46 +76,37 @@ assert
 txna ApplicationArgs 1
 btoi
 txna Accounts 1
-callsub sub2
+callsub sub3
 int 1
 return
-main_l14:
+main_l16:
 txn NumAppArgs
 int 2
 ==
 assert
-txna ApplicationArgs 1
+gtxn 1 Amount
 txn Sender
-txna ApplicationArgs 2
-callsub sub5
-return
-main_l15:
-txn Sender
-txna ApplicationArgs 1
-callsub sub4
-return
-main_l16:
-int 0
-byte "assetID"
-app_global_get
-callsub sub3
+gtxna 0 ApplicationArgs 1
+callsub sub7
 return
 main_l17:
 txn Sender
-byte "admin"
-app_local_get
-assert
-txna Accounts 1
-byte "bot"
-int 1
-app_local_put
-int 1
+txna ApplicationArgs 1
+callsub sub6
 return
 main_l18:
 int 0
+byte "assetID"
+app_global_get
+callsub sub5
 return
 main_l19:
-int 0
+byte "thisishere"
+log
+byte "bot"
+txna Accounts 1
+app_global_put
+int 1
 return
 main_l20:
 int 0
@@ -114,31 +115,67 @@ main_l21:
 int 0
 return
 main_l22:
+int 0
+return
+main_l23:
+int 1
+return
+main_l24:
 int 1
 return
 sub0: // getAssetId
 byte "assetID"
 app_global_get
 retsub
-sub1: // inner_asset_transfer
+sub1: // inner_asset_creation
+store 2
+itxn_begin
+int acfg
+itxn_field TypeEnum
+global CurrentApplicationAddress
+itxn_field ConfigAssetClawback
+global CurrentApplicationAddress
+itxn_field ConfigAssetReserve
+int 1
+itxn_field ConfigAssetDefaultFrozen
+load 2
+gtxnsa ApplicationArgs 0
+itxn_field ConfigAssetMetadataHash
+load 2
+gtxnsa ApplicationArgs 1
+itxn_field ConfigAssetName
+byte "bridge-lp"
+itxn_field ConfigAssetUnitName
+load 2
+gtxnsa ApplicationArgs 3
+btoi
+itxn_field ConfigAssetTotal
+load 2
+gtxnsa ApplicationArgs 4
+btoi
+itxn_field ConfigAssetDecimals
+itxn_submit
+itxn CreatedAssetID
+retsub
+sub2: // inner_asset_transfer
+store 8
 store 7
 store 6
 store 5
-store 4
 itxn_begin
 int axfer
 itxn_field TypeEnum
-load 4
-itxn_field XferAsset
-load 6
-itxn_field AssetSender
 load 5
-itxn_field AssetAmount
+itxn_field XferAsset
 load 7
+itxn_field AssetSender
+load 6
+itxn_field AssetAmount
+load 8
 itxn_field AssetReceiver
 itxn_submit
 retsub
-sub2: // inner_payment_txn
+sub3: // inner_payment_txn
 store 1
 store 0
 itxn_begin
@@ -152,70 +189,71 @@ load 1
 itxn_field Receiver
 itxn_submit
 retsub
-sub3: // lp_deposit_in_pool
+sub4: // setup_application
+byte "assetID"
+int 1
+callsub sub1
+app_global_put
+int 1
+retsub
+sub5: // lp_deposit_in_pool
+store 4
 store 3
-store 2
-load 2
+load 3
 gtxns Amount
 int 0
 >
-bz sub3_l2
+bz sub5_l2
+load 4
 load 3
-load 2
 gtxns Amount
 global CurrentApplicationAddress
-load 2
+load 3
 gtxns Sender
-callsub sub1
-sub3_l2:
+callsub sub2
+sub5_l2:
 int 1
 retsub
-sub4: // remove_lp_from_pool
+sub6: // remove_lp_from_pool
+store 10
 store 9
-store 8
-load 8
+load 9
 callsub sub0
 asset_holding_get AssetBalance
-store 10
 store 11
-load 10
+store 12
 load 11
-load 9
+load 12
+load 10
 >
 &&
-bnz sub4_l2
+bnz sub6_l2
 int 0
-b sub4_l3
-sub4_l2:
+b sub6_l3
+sub6_l2:
 callsub sub0
+load 10
 load 9
-load 8
 global CurrentApplicationAddress
-callsub sub1
-load 9
-load 8
 callsub sub2
+load 10
+load 9
+callsub sub3
 int 1
-sub4_l3:
+sub6_l3:
 retsub
-sub5: // use_transfer_bridge
+sub7: // use_transfer_bridge
+store 15
 store 14
 store 13
-store 12
+load 15
+log
 load 13
-byte "bridgedeposit"
-load 12
-app_local_put
-load 13
-byte "latesttimestamp"
-global LatestTimestamp
-app_local_put
-load 13
-byte "bridgereciever"
-load 14
-app_local_put
+itob
+log
 int 1
-retsub`
+retsub
+`
 
 const clearSource = `
 #pragma version 5
